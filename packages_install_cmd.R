43b5ab1635b1725484505a251371b3b7a7e432af
Amin Madoui <amadoui@genosocope.cns.fr>
2021-03-18 15:25:15 +0100

    add packages

---
+++ packages_install_cmd.R
@@ -0,0 +1,16 @@
+# install packages for the Pratical 
+
+# Fom the CRAN
+installed.packages(ggplot2)
+installed.packages(vegan)
+installed.packages(reshape2)
+
+installed.packages(MM4LMM)
+installed.packages(viridis)
+installed.packages(pcadapt)
+installed.packages(ggrepel)
+installed.packages(devtools)
+
+# Install MetaVaR from github
+library(devtools)
+install_github("madoui/metavar")
